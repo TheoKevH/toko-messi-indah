0fc92d498655a457cd687cf81f84cdfa445d4c60
TheoKevH <theodorekevinh@gmail.com>
2024-09-09 10:56:34 +0700

    fix context

--- main/views.py
+++ main/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'App Name' : 'Toko Messi Indah',
-        'Name' : 'Theodore Kevin Himawan',
-        'Class': 'PBP F',
-        'Product Name' : 'Messi Barcelona Jersey',
-        'Price' : '5000000',
-        'Description': 'A brand new Barcelona jersey of legendary player Lionel Messi',
-        'Category': 'Jersey',
+        'app_name' : 'Toko Messi Indah',
+        'name' : 'Theodore Kevin Himawan',
+        'class': 'PBP F',
+        'product_name' : 'Messi Barcelona Jersey',
+        'price' : '5000000',
+        'description': 'A brand new Barcelona jersey of legendary player Lionel Messi',
+        'category': 'Jersey',
 
     }
 
