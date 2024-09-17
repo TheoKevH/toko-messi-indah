b19a8cbad06344509216390e6b9b478c5b0b5601
TheoKevH <theodorekevinh@gmail.com>
2024-09-18 02:23:34 +0700

    add form

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   
+from main.forms import ProductForm
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    product_entries = Product.objects.all()
+
     context = {
         'app_name' : 'Toko Messi Indah',
         'name' : 'Theodore Kevin Himawan',
@@ -10,7 +14,17 @@ def show_main(request):
         'price' : '5000000',
         'description': 'A brand new Barcelona jersey of legendary player Lionel Messi',
         'category': 'Jersey',
-
+        'product_entries': product_entries 
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    
+    context = {'form': form}
+    return render(request, "create_product.html", context)
--- toko_messi_indah/settings.py
+++ toko_messi_indah/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'toko_messi_indah.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
