5762a2b9631b740cad841ce329873418046901d3
TheoKevH <theodorekevinh@gmail.com>
2024-09-27 13:37:05 +0700

    ini last login masih error

--- toko_messi_indah/settings.py
+++ toko_messi_indah/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^v$1h*x(dw@lue))hy!33t78_a=dt#m5fxhs&1rc-oa*7urc3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "theodore-kevin-tokomessiindah.pbp.cs.ui.ac.id"]
 
